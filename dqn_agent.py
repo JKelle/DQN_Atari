9376c81c8faabb13f3c3ebfb00fcc7e90a6a0dd7
Josh Kelle <joshua.a.kelle@gmail.com>
2016-12-20 23:43:05 -0600

    have separate random_agent and dqn_agent

---
+++ dqn_agent.py
@@ -0,0 +1,28 @@
+
+"""
+Work in progress.
+This agent will implement DQN.
+"""
+
+
+import random
+
+
+class DQNAgent(object):
+
+  def __init__(self, legal_action_set):
+    """
+    Args:
+      legal_action_set: list of legal actions (1D numpy array of int32)
+    """
+    self.legal_action_set = legal_action_set
+
+  def getAction(self, state):
+    """
+    Maps a state to an action.
+
+    Args:
+      state: RGB image of game, as 3D numpy array
+    """
+    return random.choice(self.legal_action_set)
+
