9cda20689f30f2a2687802c1ab2eafea86b126a9
Josh Kelle <joshua.a.kelle@gmail.com>
2016-12-24 15:34:35 -0600

    added preprocessing function

--- dqn_agent.py
+++ dqn_agent.py
@@ -6,6 +6,8 @@ This agent will implement DQN.
 
 
 import random
+import numpy as np
+from scipy.misc import imresize
 
 
 class DQNAgent(object):
@@ -26,3 +28,25 @@ class DQNAgent(object):
     """
     return random.choice(self.legal_action_set)
 
+def preprocess(cur_frame, prev_frame):
+  """
+  Preprocessing images as described in the paper.
+  1. take the max of the current frame and the previous frame
+  2. convert to Y (luminescence) channel
+  3. resize to a 84x84 image
+  """
+
+  # 1. max(prev_frame, cur_frame) because of flickering
+  frame = np.maximum(prev_frame, cur_frame)
+
+  # 2. convert to Y channel
+  # formula from http://stackoverflow.com/questions/596216/formula-to-determine-brightness-of-rgb-color
+  red = frame[:, :, 0]
+  green = frame[:, :, 1]
+  blue = frame[:, :, 2]
+  frame = 0.2126*red + 0.7152*green + 0.0722*blue
+  
+  # 3. resize to 84 x 84
+  frame = imresize(frame, (84, 84))
+
+  return frame
