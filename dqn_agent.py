9a3c4f857a7302ca06886cad03a523bcaccea6a6
Josh Kelle <joshua.a.kelle@gmail.com>
2016-12-28 00:46:11 -0600

    DQNAgent asserts networks are equal after saving and loading

--- dqn_agent.py
+++ dqn_agent.py
@@ -13,18 +13,18 @@ CHECKPOINT_DIR = "checkpoints"
 
 class DQNAgent(object):
 
-	def __init__(self, sess, checkpoint_frequency):
+	def __init__(self, sess, checkpoint_frequency, target_network_update_frequency, learning_rate):
 		self.sess = sess
 
 		# discount factor for future rewards
 		self.gamma = 0.99
-		self.target_network_update_frequency = 10000
+		self.target_network_update_frequency = target_network_update_frequency
 		self.checkpoint_frequency = checkpoint_frequency
 		self.update_counter = 0
 
 		# defines the convnet architecture in TensorFlow
-		self.prediction_network = Network("prediction")
-		self.target_network = Network("target")
+		self.prediction_network = Network("prediction", learning_rate=learning_rate)
+		self.target_network = Network("target", learning_rate=learning_rate)
 
 		self.saver = tf.train.Saver([
 			self.target_network.W_conv1,
@@ -37,11 +37,21 @@ class DQNAgent(object):
 			self.target_network.b_conv3,
 			self.target_network.b_fc1,
 			self.target_network.b_fc2,
+			self.prediction_network.W_conv1,
+			self.prediction_network.W_conv2,
+			self.prediction_network.W_conv3,
+			self.prediction_network.W_fc1,
+			self.prediction_network.W_fc2,
+			self.prediction_network.b_conv1,
+			self.prediction_network.b_conv2,
+			self.prediction_network.b_conv3,
+			self.prediction_network.b_fc1,
+			self.prediction_network.b_fc2,
 		])
 
 		ckpt = tf.train.get_checkpoint_state(CHECKPOINT_DIR)
 		if ckpt and ckpt.model_checkpoint_path:
-			self.saver.restore(sess, ckpt.model_checkpoint_path)
+			self.saver.restore(self.sess, ckpt.model_checkpoint_path)
 			self.update_counter = int(ckpt.model_checkpoint_path.split('-')[-1])
 		else:
 			print "WARNING: no checkpoint found"
@@ -60,7 +70,7 @@ class DQNAgent(object):
 		
 		# forward pass through the prediction network
 		return self.prediction_network.action.eval(
-			feed_dict={self.prediction_network.input_state:input_state})
+			feed_dict={self.prediction_network.input_state:input_state})[0]
 
 	def trainMiniBatch(self, transitions):
 		"""
@@ -98,16 +108,17 @@ class DQNAgent(object):
 
 		self.update_counter += 1
 		if self.update_counter % self.target_network_update_frequency == 0:
+			print "updating target network ..."
 			self._updateTargetNetwork()
 		
 		if self.update_counter % self.checkpoint_frequency == 0:
-			print "checkpionting ... "
+			print "checkpionting %d ... " % self.update_counter
+			self._updateTargetNetwork()
 			self.saver.save(self.sess, os.path.join(CHECKPOINT_DIR, "model"), global_step=self.update_counter)
 
 		return self.prediction_network.loss.eval(feed_dict=feed_dict)
 
 	def _updateTargetNetwork(self):
-		print "updating target network ..."
 		self.sess.run(self.target_network.W_conv1.assign(self.prediction_network.W_conv1))
 		self.sess.run(self.target_network.W_conv2.assign(self.prediction_network.W_conv2))
 		self.sess.run(self.target_network.W_conv3.assign(self.prediction_network.W_conv3))
@@ -118,3 +129,33 @@ class DQNAgent(object):
 		self.sess.run(self.target_network.b_conv3.assign(self.prediction_network.b_conv3))
 		self.sess.run(self.target_network.b_fc1.assign(self.prediction_network.b_fc1))
 		self.sess.run(self.target_network.b_fc2.assign(self.prediction_network.b_fc2))
+		self._assertSameWeights()
+
+	def _assertSameWeights(self):
+		target_weights = self.sess.run([
+			self.target_network.W_conv1,
+			self.target_network.W_conv2,
+			self.target_network.W_conv3,
+			self.target_network.W_fc1,
+			self.target_network.W_fc2,
+			self.target_network.b_conv1,
+			self.target_network.b_conv2,
+			self.target_network.b_conv3,
+			self.target_network.b_fc1,
+			self.target_network.b_fc2,
+		])
+		prediction_weights = self.sess.run([
+			self.prediction_network.W_conv1,
+			self.prediction_network.W_conv2,
+			self.prediction_network.W_conv3,
+			self.prediction_network.W_fc1,
+			self.prediction_network.W_fc2,
+			self.prediction_network.b_conv1,
+			self.prediction_network.b_conv2,
+			self.prediction_network.b_conv3,
+			self.prediction_network.b_fc1,
+			self.prediction_network.b_fc2,
+		])
+
+		for target_tensor, prediction_tensor in zip(target_weights, prediction_weights):
+			assert (target_tensor == prediction_tensor).all()
