8b0b3d1cdf0f79896a69af51a2be0a98f5891fcc
Josh Kelle <joshua.a.kelle@gmail.com>
2017-01-02 16:32:48 -0600

    updated run.py

--- run.py
+++ run.py
@@ -5,23 +5,34 @@
 # This is a direct port to python of the shared library example from
 # ALE provided in doc/examples/sharedLibraryInterfaceExample.cpp
 
+
+import argparse
+from collections import deque
+import random
 import sys
-from random import randrange
+
+# import the Atari emulator
 from ale_python_interface import ALEInterface
+import numpy as np
+import tensorflow as tf
+
+# import agents
 from dqn_agent import DQNAgent
 from baseline_agent import BaselineAgent
 from random_agent import RandomAgent
+from utils import preprocess
 
 
 ale = ALEInterface()
 
-# Get & Set the desired settings
 ale.setInt(b'random_seed', 123)
+
+LEGAL_ACTIONS = [1, 11, 12]
 
 # Set USE_SDL to true to display the screen. ALE must be compilied
 # with SDL enabled for this to work. On OSX, pygame init is used to
 # proxy-call SDL_main.
-USE_SDL = False
+USE_SDL = True
 if USE_SDL:
     if sys.platform == 'darwin':
         import pygame
@@ -31,38 +42,117 @@ if USE_SDL:
         ale.setBool('sound', True)
     ale.setBool('display_screen', True)
 
-# Load the ROM file
-
-if len(sys.argv) < 2:
-    print("No ROM file specified. Using breakout.bin as default")
-    rom_file = "roms/breakout.bin"
-else:
-    rom_file = str.encode(sys.argv[1])
-
+rom_file = "roms/breakout.bin"
 ale.loadROM(rom_file)
 
-# Get the list of legal actions
-# myagent = DQNAgent(ale.getLegalActionSet())
-# myagent = RandomAgent(ale.getLegalActionSet())
-myagent = BaselineAgent()
+num_episodes=1
+num_repeat_action=4
+frames_per_state=4
+def play(agent, num_episodes=1, num_repeat_action=4, frames_per_state=4, epsilon=0.05):
+    """
+    Actions:
+         0 : no ball, no movement
 
-# Play 10 episodes
-for episode in range(10):
-    total_reward = 0
-    
-    # play one game
-    while not ale.game_over():
-        # game state is just the pixels of the screen
-        state = ale.getScreenRGB()
+         1 : starts ball, no movement
+
+         2 : no ball, no movement
+         3 : no ball, moves to the right
+         4 : no ball, moves to the left
+         5 : no ball, no movement
+         6 : no ball, moves to the right
+         7 : no ball, moves to the left
+         8 : no ball, moves to the right
+         9 : no ball, moves to the left
         
-        # the agent maps states to actions
-        action = myagent.getAction(state)
+        10 : starts the ball, doesn't move
+        11 : starts the ball, moves to the right
+        12 : starts the ball, moves to the left
+        13 : starts the ball, doesn't move
+        14 : starts the ball, moves to the right
+        15 : starts the ball, moves to the left
+        16 : starts the ball, moves to the right
+        17 : starts the ball, moves to the left
+    """
+    recent_frames = deque([], frames_per_state)
+
+    # Play 10 episodes ("episode" = one game)
+    for episode in xrange(num_episodes):
+        total_reward = 0
         
-        # Apply an action and get the resulting reward
-        reward = ale.act(action);
-        total_reward += reward
+        # reset the game
+        ale.reset_game()
+        is_new_game = True
+        
+        # for the first frame, just copy the same frame four times
+        cur_frame = ale.getScreenRGB()
+        cur_state = np.stack([preprocess(cur_frame)]*frames_per_state, axis=2)
+        assert cur_state.shape == (84, 84, 4)
+        
+        # play one game
+        while not ale.game_over():
+            
+            # # epsilon greedy
+            # if is_new_game:
+            #     action = 1  # serve the ball, don't move the paddle
+            #     is_new_game = False
+            if random.random() < epsilon:
+                # take a random action
+                action = random.choice(LEGAL_ACTIONS)
+                # print "chose action", action, "*"
+            else:
+                # choose action according the DQN policy
+                action_index = agent.getAction(cur_state)
+                action = LEGAL_ACTIONS[action_index]
+                # print "chose action", action
+
+
+            # take the action 4 times
+            reward = 0
+            for _ in range(num_repeat_action):
+                prev_frame = cur_frame
+                reward += ale.act(action)
+                cur_frame = ale.getScreenRGB()
+                recent_frames.append(preprocess(cur_frame, prev_frame))
+
+            # clip reward to range [-1, 1]
+            reward = min(reward, 1)
+            reward = max(reward, -1)
+            total_reward += reward
+
+            # stack the most recent 4 frames
+            cur_state = np.stack(recent_frames, axis=2)
+        
+        print('Episode %d ended with score: %d' % (episode, total_reward))
+        
+        # reset the game to run another episode
+        ale.reset_game()
+
+
+def main(sess):
+    possible_agents = {
+        "dqn": DQNAgent(sess, 100000, 100000, 0.00001),
+        "random": RandomAgent(LEGAL_ACTIONS),
+        "baseline": BaselineAgent(),
+    }
+
+    parser = argparse.ArgumentParser(
+        description="This program runs the Atari game Breakout with an AI agent.")
     
-    print('Episode %d ended with score: %d' % (episode, total_reward))
+    parser.add_argument(
+        '-a', '--agent',
+        default='dqn',
+        choices=possible_agents.keys(),
+        dest='agent_type',
+        help='specifies the type of agent used to play the game')
+
+    args = parser.parse_args()
+
+    agent = possible_agents[args.agent_type]
     
-    # reset the game to run another episode
-    ale.reset_game()
+    play(agent, num_episodes=10, epsilon=0.0)
+
+
+
+if __name__ == '__main__':
+    with tf.Session() as sess:
+        main(sess)
