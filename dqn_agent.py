314eedfdde9c208c08efe89922c435269690d6f3
Josh Kelle <joshua.a.kelle@gmail.com>
2017-01-02 19:50:34 -0600

    converted tabs to spaces

--- dqn_agent.py
+++ dqn_agent.py
@@ -14,162 +14,162 @@ CHECKPOINT_DIR = "/vision/vision_users/jkelle/checkpointReducedActionSet"
 
 class DQNAgent(object):
 
-	def __init__(self, sess, checkpoint_frequency, target_network_update_frequency, learning_rate):
-		self.sess = sess
+    def __init__(self, sess, checkpoint_frequency, target_network_update_frequency, learning_rate):
+        self.sess = sess
 
-		# discount factor for future rewards
-		self.gamma = 0.99
-		self.target_network_update_frequency = target_network_update_frequency
-		self.checkpoint_frequency = checkpoint_frequency
-		self.counter = 0
+        # discount factor for future rewards
+        self.gamma = 0.99
+        self.target_network_update_frequency = target_network_update_frequency
+        self.checkpoint_frequency = checkpoint_frequency
+        self.counter = 0
 
-		# defines the convnet architecture in TensorFlow
-		self.prediction_network = Network("prediction", learning_rate=learning_rate)
-		self.target_network = Network("target", learning_rate=learning_rate)
+        # defines the convnet architecture in TensorFlow
+        self.prediction_network = Network("prediction", learning_rate=learning_rate)
+        self.target_network = Network("target", learning_rate=learning_rate)
 
-		self.sess.run(tf.global_variables_initializer())
+        self.sess.run(tf.global_variables_initializer())
 
-		self.saver = tf.train.Saver(
-			[
-				self.target_network.W_conv1,
-				self.target_network.W_conv2,
-				self.target_network.W_conv3,
-				self.target_network.W_fc1,
-				self.target_network.W_fc2,
-				self.target_network.b_conv1,
-				self.target_network.b_conv2,
-				self.target_network.b_conv3,
-				self.target_network.b_fc1,
-				self.target_network.b_fc2,
-				self.prediction_network.W_conv1,
-				self.prediction_network.W_conv2,
-				self.prediction_network.W_conv3,
-				self.prediction_network.W_fc1,
-				self.prediction_network.W_fc2,
-				self.prediction_network.b_conv1,
-				self.prediction_network.b_conv2,
-				self.prediction_network.b_conv3,
-				self.prediction_network.b_fc1,
-				self.prediction_network.b_fc2,
-			],
-			max_to_keep=2
-		)
+        self.saver = tf.train.Saver(
+            [
+                self.target_network.W_conv1,
+                self.target_network.W_conv2,
+                self.target_network.W_conv3,
+                self.target_network.W_fc1,
+                self.target_network.W_fc2,
+                self.target_network.b_conv1,
+                self.target_network.b_conv2,
+                self.target_network.b_conv3,
+                self.target_network.b_fc1,
+                self.target_network.b_fc2,
+                self.prediction_network.W_conv1,
+                self.prediction_network.W_conv2,
+                self.prediction_network.W_conv3,
+                self.prediction_network.W_fc1,
+                self.prediction_network.W_fc2,
+                self.prediction_network.b_conv1,
+                self.prediction_network.b_conv2,
+                self.prediction_network.b_conv3,
+                self.prediction_network.b_fc1,
+                self.prediction_network.b_fc2,
+            ],
+            max_to_keep=2
+        )
 
-		ckpt = tf.train.get_checkpoint_state(CHECKPOINT_DIR)
-		if ckpt and ckpt.model_checkpoint_path:
-			print "loading checkpoint parameters ..."
-			self.saver.restore(self.sess, ckpt.model_checkpoint_path)
-			self.counter = int(ckpt.model_checkpoint_path.split('-')[-1])
-			print "done"
-		else:
-			print "WARNING: no checkpoint found. Starting weights from scratch."
+        ckpt = tf.train.get_checkpoint_state(CHECKPOINT_DIR)
+        if ckpt and ckpt.model_checkpoint_path:
+            print "loading checkpoint parameters ..."
+            self.saver.restore(self.sess, ckpt.model_checkpoint_path)
+            self.counter = int(ckpt.model_checkpoint_path.split('-')[-1])
+            print "done"
+        else:
+            print "WARNING: no checkpoint found. Starting weights from scratch."
 
-		# self._assertSameWeights()
+        # self._assertSameWeights()
 
-	def getCounter(self):
-		return self.counter
+    def getCounter(self):
+        return self.counter
 
-	def getAction(self, state):
-		"""
-		Maps a single state to a single action.
+    def getAction(self, state):
+        """
+        Maps a single state to a single action.
 
-		Args:
-			state: a numpy array with shape
-		"""
-		# reshape [84, 84, 4] to be [1, 84, 84, 4]
-		# and convert from uint8 to float32
-		input_state = np.array([state], dtype=np.float32)
-		
-		# forward pass through the prediction network
-		return self.prediction_network.action.eval(
-			feed_dict={self.prediction_network.input_state:input_state})[0]
+        Args:
+            state: a numpy array with shape
+        """
+        # reshape [84, 84, 4] to be [1, 84, 84, 4]
+        # and convert from uint8 to float32
+        input_state = np.array([state], dtype=np.float32)
 
-	def trainMiniBatch(self, transitions):
-		"""
-		Args:
-			transitions: list of (state1, action, reward, state2) tuples
-				- state1: numpy array with shape (84, 84, 4)
-				- action: scalar int in range [0, NUM_ACTIONS]
-				- reward: scalar float in range [-1, 1]
-				- state2: numpy array with shape (84, 84, 4)
+        # forward pass through the prediction network
+        return self.prediction_network.action.eval(
+            feed_dict={self.prediction_network.input_state:input_state})[0]
 
-		Returns:
-			loss
-		"""
-		# reorganize data
-		state1, actions, rewards, state2 = zip(*transitions)
-		state1 = np.array(state1, dtype=np.float32)
-		actions = np.array(actions, dtype=np.uint8)
-		rewards = np.array(rewards, dtype=np.float32)
-		state2 = np.array(state2, dtype=np.float32)
+    def trainMiniBatch(self, transitions):
+        """
+        Args:
+            transitions: list of (state1, action, reward, state2) tuples
+                - state1: numpy array with shape (84, 84, 4)
+                - action: scalar int in range [0, NUM_ACTIONS]
+                - reward: scalar float in range [-1, 1]
+                - state2: numpy array with shape (84, 84, 4)
 
-		# forward pass through the target network
-		target_q_values = self.target_network.q_values.eval(
-			feed_dict={self.target_network.input_state:state2})
+        Returns:
+            loss
+        """
+        # reorganize data
+        state1, actions, rewards, state2 = zip(*transitions)
+        state1 = np.array(state1, dtype=np.float32)
+        actions = np.array(actions, dtype=np.uint8)
+        rewards = np.array(rewards, dtype=np.float32)
+        state2 = np.array(state2, dtype=np.float32)
 
-		feed_dict = {
-			self.prediction_network.input_state: state1,
-			self.prediction_network.actions: actions,
-			self.prediction_network.rewards: rewards,
-			self.prediction_network.target_q_values: target_q_values
-		}
+        # forward pass through the target network
+        target_q_values = self.target_network.q_values.eval(
+            feed_dict={self.target_network.input_state:state2})
 
-		self.prediction_network.train_step.run(feed_dict=feed_dict)
+        feed_dict = {
+            self.prediction_network.input_state: state1,
+            self.prediction_network.actions: actions,
+            self.prediction_network.rewards: rewards,
+            self.prediction_network.target_q_values: target_q_values
+        }
 
-		self.counter += 1
-		
-		# update target network
-		if self.counter % self.target_network_update_frequency == 0:
-			print "updating target network ..."
-			self._updateTargetNetwork()
-		
-		# checkpoint (save weights to disk)
-		if self.counter % self.checkpoint_frequency == 0:
-			print "checkpointing %d ... " % self.counter
-			self.saver.save(self.sess, os.path.join(CHECKPOINT_DIR, "model"), global_step=self.counter)
+        self.prediction_network.train_step.run(feed_dict=feed_dict)
 
-		return self.prediction_network.loss.eval(feed_dict=feed_dict)
+        self.counter += 1
 
-	def _updateTargetNetwork(self):
-		self.sess.run(self.target_network.W_conv1.assign(self.prediction_network.W_conv1))
-		self.sess.run(self.target_network.W_conv2.assign(self.prediction_network.W_conv2))
-		self.sess.run(self.target_network.W_conv3.assign(self.prediction_network.W_conv3))
-		self.sess.run(self.target_network.W_fc1.assign(self.prediction_network.W_fc1))
-		self.sess.run(self.target_network.W_fc2.assign(self.prediction_network.W_fc2))
-		self.sess.run(self.target_network.b_conv1.assign(self.prediction_network.b_conv1))
-		self.sess.run(self.target_network.b_conv2.assign(self.prediction_network.b_conv2))
-		self.sess.run(self.target_network.b_conv3.assign(self.prediction_network.b_conv3))
-		self.sess.run(self.target_network.b_fc1.assign(self.prediction_network.b_fc1))
-		self.sess.run(self.target_network.b_fc2.assign(self.prediction_network.b_fc2))
-		# self._assertSameWeights()
+        # update target network
+        if self.counter % self.target_network_update_frequency == 0:
+            print "updating target network ..."
+            self._updateTargetNetwork()
 
-	def _assertSameWeights(self):
-		target_weights = self.sess.run([
-			self.target_network.W_conv1,
-			self.target_network.W_conv2,
-			self.target_network.W_conv3,
-			self.target_network.W_fc1,
-			self.target_network.W_fc2,
-			self.target_network.b_conv1,
-			self.target_network.b_conv2,
-			self.target_network.b_conv3,
-			self.target_network.b_fc1,
-			self.target_network.b_fc2,
-		])
-		prediction_weights = self.sess.run([
-			self.prediction_network.W_conv1,
-			self.prediction_network.W_conv2,
-			self.prediction_network.W_conv3,
-			self.prediction_network.W_fc1,
-			self.prediction_network.W_fc2,
-			self.prediction_network.b_conv1,
-			self.prediction_network.b_conv2,
-			self.prediction_network.b_conv3,
-			self.prediction_network.b_fc1,
-			self.prediction_network.b_fc2,
-		])
+        # checkpoint (save weights to disk)
+        if self.counter % self.checkpoint_frequency == 0:
+            print "checkpointing %d ... " % self.counter
+            self.saver.save(self.sess, os.path.join(CHECKPOINT_DIR, "model"), global_step=self.counter)
 
-		for target_tensor, prediction_tensor in zip(target_weights, prediction_weights):
-			assert (target_tensor == prediction_tensor).all()
+        return self.prediction_network.loss.eval(feed_dict=feed_dict)
 
-		print "True"
+    def _updateTargetNetwork(self):
+        self.sess.run(self.target_network.W_conv1.assign(self.prediction_network.W_conv1))
+        self.sess.run(self.target_network.W_conv2.assign(self.prediction_network.W_conv2))
+        self.sess.run(self.target_network.W_conv3.assign(self.prediction_network.W_conv3))
+        self.sess.run(self.target_network.W_fc1.assign(self.prediction_network.W_fc1))
+        self.sess.run(self.target_network.W_fc2.assign(self.prediction_network.W_fc2))
+        self.sess.run(self.target_network.b_conv1.assign(self.prediction_network.b_conv1))
+        self.sess.run(self.target_network.b_conv2.assign(self.prediction_network.b_conv2))
+        self.sess.run(self.target_network.b_conv3.assign(self.prediction_network.b_conv3))
+        self.sess.run(self.target_network.b_fc1.assign(self.prediction_network.b_fc1))
+        self.sess.run(self.target_network.b_fc2.assign(self.prediction_network.b_fc2))
+        # self._assertSameWeights()
+
+    def _assertSameWeights(self):
+        target_weights = self.sess.run([
+            self.target_network.W_conv1,
+            self.target_network.W_conv2,
+            self.target_network.W_conv3,
+            self.target_network.W_fc1,
+            self.target_network.W_fc2,
+            self.target_network.b_conv1,
+            self.target_network.b_conv2,
+            self.target_network.b_conv3,
+            self.target_network.b_fc1,
+            self.target_network.b_fc2,
+        ])
+        prediction_weights = self.sess.run([
+            self.prediction_network.W_conv1,
+            self.prediction_network.W_conv2,
+            self.prediction_network.W_conv3,
+            self.prediction_network.W_fc1,
+            self.prediction_network.W_fc2,
+            self.prediction_network.b_conv1,
+            self.prediction_network.b_conv2,
+            self.prediction_network.b_conv3,
+            self.prediction_network.b_fc1,
+            self.prediction_network.b_fc2,
+        ])
+
+        for target_tensor, prediction_tensor in zip(target_weights, prediction_weights):
+            assert (target_tensor == prediction_tensor).all()
+
+        print "True"
--- network.py
+++ network.py
@@ -16,80 +16,80 @@ def bias_variable(shape):
 
 
 class Network(object):
-	def __init__(self, name, gamma=0.99, learning_rate=0.00025, momentum=0.95):
-		"""
-		Args:
-			includeLoss: boolean, if true, computatin graph will include ops
-				for computing loss and training
-		"""
-		self.name = name
+    def __init__(self, name, gamma=0.99, learning_rate=0.00025, momentum=0.95):
+        """
+        Args:
+            includeLoss: boolean, if true, computatin graph will include ops
+                for computing loss and training
+        """
+        self.name = name
 
-		with tf.variable_scope(self.name):
-			# data layer
-			self.input_state = tf.placeholder(tf.float32, shape=[None, 84, 84, 4])
+        with tf.variable_scope(self.name):
+            # data layer
+            self.input_state = tf.placeholder(tf.float32, shape=[None, 84, 84, 4])
 
-			# conv2 layer
-			self.W_conv1 = weight_variable([8, 8, 4, 32])
-			self.b_conv1 = bias_variable([32])
-			h_conv1 = tf.nn.relu(tf.nn.conv2d(self.input_state, self.W_conv1, strides=[1, 4, 4, 1], padding="SAME") + self.b_conv1)
-			# image is now 21 x 21 x 32
+            # conv2 layer
+            self.W_conv1 = weight_variable([8, 8, 4, 32])
+            self.b_conv1 = bias_variable([32])
+            h_conv1 = tf.nn.relu(tf.nn.conv2d(self.input_state, self.W_conv1, strides=[1, 4, 4, 1], padding="SAME") + self.b_conv1)
+            # image is now 21 x 21 x 32
 
-			# conv2 layer
-			self.W_conv2 = weight_variable([4, 4, 32, 64])
-			self.b_conv2 = bias_variable([64])
-			h_conv2 = tf.nn.relu(tf.nn.conv2d(h_conv1, self.W_conv2, strides=[1, 2, 2, 1], padding="SAME") + self.b_conv2)
-			# image is now 11 x 11 x 64
+            # conv2 layer
+            self.W_conv2 = weight_variable([4, 4, 32, 64])
+            self.b_conv2 = bias_variable([64])
+            h_conv2 = tf.nn.relu(tf.nn.conv2d(h_conv1, self.W_conv2, strides=[1, 2, 2, 1], padding="SAME") + self.b_conv2)
+            # image is now 11 x 11 x 64
 
-			# conv3 layer
-			self.W_conv3 = weight_variable([3, 3, 64, 64])
-			self.b_conv3 = bias_variable([64])
-			h_conv3 = tf.nn.relu(tf.nn.conv2d(h_conv2, self.W_conv3, strides=[1, 1, 1, 1], padding="SAME") + self.b_conv3)
-			# image is now 11 x 11 x 64
+            # conv3 layer
+            self.W_conv3 = weight_variable([3, 3, 64, 64])
+            self.b_conv3 = bias_variable([64])
+            h_conv3 = tf.nn.relu(tf.nn.conv2d(h_conv2, self.W_conv3, strides=[1, 1, 1, 1], padding="SAME") + self.b_conv3)
+            # image is now 11 x 11 x 64
 
-			# fc1 layer
-			h_conv3_flat = tf.reshape(h_conv3, [-1, 11 * 11 * 64])
-			self.W_fc1 = weight_variable([11 * 11 * 64, 512])
-			self.b_fc1 = bias_variable([512])
-			h_fc1 = tf.nn.relu(tf.matmul(h_conv3_flat, self.W_fc1) + self.b_fc1)
+            # fc1 layer
+            h_conv3_flat = tf.reshape(h_conv3, [-1, 11 * 11 * 64])
+            self.W_fc1 = weight_variable([11 * 11 * 64, 512])
+            self.b_fc1 = bias_variable([512])
+            h_fc1 = tf.nn.relu(tf.matmul(h_conv3_flat, self.W_fc1) + self.b_fc1)
 
-			# keep_prob = tf.placeholder(tf.float32)
-			# h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+            # keep_prob = tf.placeholder(tf.float32)
+            # h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
-			# fc2 layer
-			self.W_fc2 = weight_variable([512, NUM_ACTIONS])
-			self.b_fc2 = bias_variable([NUM_ACTIONS])
+            # fc2 layer
+            self.W_fc2 = weight_variable([512, NUM_ACTIONS])
+            self.b_fc2 = bias_variable([NUM_ACTIONS])
 
-			# distribution of actions
-			self.q_values = tf.matmul(h_fc1, self.W_fc2) + self.b_fc2
+            # distribution of actions
+            self.q_values = tf.matmul(h_fc1, self.W_fc2) + self.b_fc2
 
-			# selects the  single action with max value
-			self.action = tf.argmax(self.q_values, 1)
+            # selects the  single action with max value
+            self.action = tf.argmax(self.q_values, 1)
 
-			##############
-			# Loss layer #
-			##############
+            ##############
+            # Loss layer #
+            ##############
 
-			# placeholder for rewards and actions, from transitions
-			self.rewards = tf.placeholder(tf.float32, shape=[None])
-			self.actions = tf.placeholder(tf.uint8, shape=[None])
+            # placeholder for rewards and actions, from transitions
+            self.rewards = tf.placeholder(tf.float32, shape=[None])
+            self.actions = tf.placeholder(tf.uint8, shape=[None])
 
-			# placeholder for Q values from target network
-			self.target_q_values = tf.placeholder(tf.float32, shape=[None, NUM_ACTIONS])
+            # placeholder for Q values from target network
+            self.target_q_values = tf.placeholder(tf.float32, shape=[None, NUM_ACTIONS])
 
-			# compute: r + gamma * max_{a'} Q_target(s, a')
-			max_target_q_values = tf.reduce_max(self.target_q_values, axis=1)
-			discounted_target_q_values = tf.scalar_mul(gamma, max_target_q_values)
-			target_values = tf.add(self.rewards, discounted_target_q_values)
+            # compute: r + gamma * max_{a'} Q_target(s, a')
+            max_target_q_values = tf.reduce_max(self.target_q_values, axis=1)
+            discounted_target_q_values = tf.scalar_mul(gamma, max_target_q_values)
+            target_values = tf.add(self.rewards, discounted_target_q_values)
 
-			# compute the Q value of the actions taken in the first state
-			observed_values = tf.reduce_sum(tf.mul(self.q_values, tf.one_hot(self.actions, NUM_ACTIONS)), axis=1)
+            # compute the Q value of the actions taken in the first state
+            observed_values = tf.reduce_sum(tf.mul(self.q_values, tf.one_hot(self.actions, NUM_ACTIONS)), axis=1)
 
-			# compute the loss - uses Huber to clip gradient
-			err = tf.sub(target_values, observed_values)
-			self.loss = tf.select(tf.abs(err) < 1.0, 0.5 * tf.square(err), tf.abs(err) - 0.5)
+            # compute the loss - uses Huber to clip gradient
+            err = tf.sub(target_values, observed_values)
+            self.loss = tf.select(tf.abs(err) < 1.0, 0.5 * tf.square(err), tf.abs(err) - 0.5)
 
-			######################
-			# Training optimizer #
-			######################
+            ######################
+            # Training optimizer #
+            ######################
 
-			self.train_step = tf.train.RMSPropOptimizer(learning_rate, momentum=momentum).minimize(self.loss)
+            self.train_step = tf.train.RMSPropOptimizer(learning_rate, momentum=momentum).minimize(self.loss)
